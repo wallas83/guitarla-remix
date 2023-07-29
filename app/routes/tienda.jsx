a17f32acfea0bce181412a3b40f57d87cebfc067
wallas <whuanca06@gmail.com>
2023-07-29 00:44:05 -0400

    adding a guitar tienda

--- app/routes/tienda.jsx
+++ app/routes/tienda.jsx
@@ -1,14 +1,55 @@
+import { useLoaderData } from '@remix-run/react';
+import { getGuitarras } from "../models/guitarras.server";
+import Guitarra from '../components/guitarra';
+import styles from '../styles/guitarras.css'
 
+export function meta (){
+  return[
+    {
+      title:'GuitarLa - Tienda de guitarras',
+      description: 'GuitarLa - Nuestra coleccion de guitarras'
+    }
+  ]
+}
+export function links() {
+  return [
+    {
+      rel: 'stylesheet',
+      href: styles
+    }
+  ]
+}
 export async function loader() {
-  const  respuesta = await fetch(`${process.env.API_URL}/guitarras/?populate=imagen`);
-  const resultado =  await respuesta.json();
-  console.log(resultado);
-  console.log(process.env.API_URL);
-  return {};
+
+  const guitarras = await getGuitarras();
+
+  return guitarras.data;
 }
 function Tienda() {
+  const guitarras = useLoaderData();
+  //  console.log(guitarras.length);
   return (
-    <div>Tienda</div>
+    <main className='contenedor'>
+      <h2 className='heading'>Nuestra colección</h2>
+      {
+
+        guitarras.length && (
+          <div className='guitarras-grid'>
+            {guitarras.map(guitarra => (
+              <Guitarra 
+                key={guitarra?.id}
+                guitarra={guitarra?.attributes}
+              />
+            )
+
+
+
+
+            )}
+          </div>
+        )
+      }
+    </main>
   )
 }
 
